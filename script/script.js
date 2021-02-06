d7a2403036a755e80b74297fccf043f2b2ca98ae
San4es <mister.San4es@list.ru>
2021-02-06 10:36:05 +0400

    lesson06 deb

--- script/script.js
+++ script/script.js
@@ -8,19 +8,22 @@ let game = function (n) {
   let number = 35;
   let guessNumber = prompt('Угадай число от 1 до 100');
 
-  if (guessNumber === null) {
-    alert('Игра окончена');
-  } else if (guessNumber < number) {
-    alert('Загаданное число больше');
-    game();
-  } else if (!isNumber(guessNumber)) {
-    alert('Введи число!');
-    game();
-  } else if (guessNumber > number) {
-    alert('Загаданное число меньше');
-    game();
-  } else if (+guessNumber === number) {
-    alert('Поздравляю, Вы угадали!!!');
+  function start() {
+    if (guessNumber === null) {
+      alert('Игра окончена');
+    } else if (guessNumber < number) {
+      alert('Загаданное число больше');
+      game();
+    } else if (!isNumber(guessNumber)) {
+      alert('Введи число!');
+      game();
+    } else if (guessNumber > number) {
+      alert('Загаданное число меньше');
+      game();
+    } else if (+guessNumber === number) {
+      alert('Поздравляю, Вы угадали!!!');
+    }
   }
+  start();
 };
 game();
