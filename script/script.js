1d75541e04d535b851b40baeba4bd15f8c1c8a9f
San4es <mister.San4es@list.ru>
2021-02-22 21:54:37 +0400

    lesson14_1

--- script/script.js
+++ script/script.js
@@ -1,29 +1,115 @@
 'use strict';
 
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+// let inTagName = document.querySelector('.inTagName');
+// let tagNameBtn = document.querySelector('.tagNameBtn');
 
-let game = function (n) {
-  let number = 35;
-  let guessNumber = prompt('Угадай число от 1 до 100');
+// function DomElement(selector, height, width, bg, fontSize) {
+//   this.selector = selector;
+//   this.height = height;
+//   this.width = width;
+//   this.bg = bg;
+//   this.fontSize = fontSize;
+// }
 
-  function start() {
-    if (guessNumber === null) {
-      alert('Игра окончена');
-    } else if (guessNumber < number) {
-      alert('Загаданное число больше');
-      game();
-    } else if (!isNumber(guessNumber)) {
-      alert('Введи число!');
-      game();
-    } else if (guessNumber > number) {
-      alert('Загаданное число меньше');
-      game();
-    } else if (+guessNumber === number) {
-      alert('Поздравляю, Вы угадали!!!');
+// tagNameBtn.addEventListener('click', function () {
+//   let newBlock = document.createElement('div');
+//   document.body.appendChild(newBlock);
+
+//   if (inTagName.value[0] === '.') {
+//     newBlock.classList.add(inTagName.value.slice(1));
+//   } else if (inTagName.value[0] === '#') {
+//     newBlock.id = inTagName.value.slice(1);
+//   }
+
+//   newBlock.style.cssText = `
+//     background-color: yellow;
+//     width: 100px;
+//     height: 100px;
+//     text-align: center;
+//   `;
+
+// });
+
+
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+}
+
+DomElement.prototype.NewDomElement = function () {
+  let newBlock = document.createElement('div');
+  document.body.appendChild(newBlock);
+
+//   for (let key in this) {
+
+//     if (this[key][0] === '.') {
+//       newBlock.classList.add(this[key].slice(1));      
+
+//     } else if (this[key][0] === '#') {
+//       newBlock.id = (this[key].slice(1));
+      
+//     }
+
+// newBlock.style.cssText = `
+//         height: 100px;
+//         width: 100px;
+//         background-color: red;
+//         font-size: 30px;
+//       `;
+
+// console.log(this[key]);
+    // console.log('newBlock: ', newBlock);
+
+  // }
+
+
+const keys = Object.keys(this);
+for (let key of keys) {
+  // console.log(`key = ${key}, value = ${this[key]}`);
+  newBlock.style.cssText = `
+  ${key}: ${this[key]}
+  `;
+
+  if (key === 'selector' ){
+    if(this[key][0] === '.') {
+      newBlock.classList.add(this[key].slice(1));
+    console.log(`${key}: ${this[key].slice(1)};`);
+    } else if(this[key][0] === '#'){
+      newBlock.id = (this[key].slice(1));
+      console.log(`${key}: ${this[key].slice(1)};`);
     }
+    
+  } 
+  else if (key === 'height' ){
+    newBlock.style.cssText = `${this[key]};`;
+    console.log(`${key}: ${this[key]};`);
+  } 
+  else if (key === 'width' ){
+    newBlock.style.cssText = `${this[key]}`;
+    console.log(`${key}: ${this[key]};`);
   }
-  start();
+  else if (key === 'bg' ){
+    newBlock.style.cssText = `${this[key]}`;
+    console.log(`background-color: ${this[key]};`);
+  }
+  else if (key === 'fontSize' ){
+    newBlock.style.cssText = `${this[key]}`;
+    console.log(`font-size: ${this[key]};`);
+  }
+
+}
+
+
 };
-game();
+
+
+let newB = new DomElement('.block', '200px', '200px', 'red', '22px');
+console.log('newB: ', newB);
+newB.NewDomElement();
+
+let newB2 = new DomElement('#best', '200px', '200px', 'blue', '22px');
+console.log('newB2: ', newB2);
+newB2.NewDomElement();
